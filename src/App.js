bf6e34819064ac2bb01e8700612db60c1e5cca6d
Karolin Rafalski <kr2174@caa.columbia.edu>
2019-05-05 12:14:51 -0400

    finish build

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import './css/normalize.css'
 import './css/skeleton.css'
 import './css/index.css'
 import ballons from './images/two-balloon-icons-68911.png'
-// import pencil from './images/simpleiconDOTcom-pen-15-64x64.png'
+import pencil from './images/simpleiconDOTcom-pen-15-64x64.png'
 import NewForm from './components/NewForm.js'
 import Show from './components/Show.js'
-// import UpdateForm from './components/UpdateForm.js'
+import UpdateForm from './components/UpdateForm.js'
 let baseURL = process.env.REACT_APP_BASEURL
 
 //alternate baseURL = 'https://fathomless-sierra-68956.herokuapp.com'
@@ -24,6 +24,8 @@ class App extends React.Component {
    this.getHolidays = this.getHolidays.bind(this)
    this.handleAddHoliday = this.handleAddHoliday.bind(this)
    this.toggleCelebrated = this.toggleCelebrated.bind(this)
+   this.toggleUpdateForm = this.toggleUpdateForm.bind(this)
+   this.updateHoliday = this.updateHoliday.bind(this)
  }
  componentDidMount(){
   this.getHolidays()
@@ -72,6 +74,24 @@ getHoliday(holiday) {
         this.setState({holidays: copyHolidays})
   })
 }
+toggleUpdateForm() {
+  this.setState({showForm: !this.state.showForm})
+}
+updateHoliday(holiday) {
+ fetch(baseURL + '/holidays/' + holiday.id, {
+   method: 'PUT',
+   body: JSON.stringify(holiday),
+   headers: {
+     'Content-Type' : 'application/json'
+   }
+ }).then(res => res.json())
+ .then(resJson => {
+      const copyHolidays = [...this.state.holidays]
+       const findIndex = this.state.holidays.findIndex(holiday => holiday._id === resJson._id)
+       copyHolidays[findIndex]= resJson
+       this.setState({holidays: copyHolidays})
+ })
+}
   render () {
    return (
      <div className='container'>
@@ -82,15 +102,16 @@ getHoliday(holiday) {
           { this.state.holidays.map(holiday => {
               return (
                 <tr key={holiday._id} onMouseOver={() => this.getHoliday(holiday)}>
-                <td
-                  onDoubleClick={() => this.toggleCelebrated(holiday)}
-                  className={holiday.celebrated
-                    ? 'celebrated'
-                    :
-                    null}
-                >{holiday.name} Day</td>
-                <td>{holiday.likes}</td>
-                <td onClick={() => this.updateLikes(holiday)}><img src={ballons} alt="ballons"/></td>
+                  <td
+                    onDoubleClick={() => this.toggleCelebrated(holiday)}
+                    className={holiday.celebrated
+                      ? 'celebrated'
+                      :
+                      null}
+                    >{holiday.name} Day</td>
+                  <td>{holiday.likes}</td>
+                  <td onClick={() => this.updateLikes(holiday)}><img src={ballons} alt="ballons"/></td>
+                  <td><img src={pencil} alt="pencil" onClick={this.toggleUpdateForm}/></td>
                   <td onClick={()=>this.deleteHoliday(holiday._id)}>X</td>
                 </tr>
               )
@@ -101,6 +122,11 @@ getHoliday(holiday) {
       { this.state.holiday
         ? <Show holiday={this.state.holiday}/>
         : null }
+        { this.state.showForm
+          ? <UpdateForm holiday={this.state.holiday}             toggleUpdateForm={this.toggleUpdateForm}
+          handleUpdateHoliday={this.updateHoliday}
+          />
+          : null }
      </div>
    )
  }
